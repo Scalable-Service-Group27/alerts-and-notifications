fe5270f24515d15a70b7d902871a6059ae8ff837
Chilamakurthi, Usha <Usha.Chilamakurthi@Honeywell.com>
2025-04-26 17:50:10 +0530

    alerts-notification basic code

---
+++ src/main/java/com/alerts/AlertsNotificationApplication.java
@@ -0,0 +1,11 @@
+package com.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class AlertsNotificationApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AlertsNotificationApplication.class, args);
+    }
+}
