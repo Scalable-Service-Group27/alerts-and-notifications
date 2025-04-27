a66303e55a0f6c8e65d5b31d99b08eb0ebf0c60e
Chilamakurthi, Usha <Usha.Chilamakurthi@Honeywell.com>
2025-04-27 16:23:18 +0530

    notification repo code

---
+++ src/main/java/com/fraud_detection/notification/entity/UserDetailsEntity.java
@@ -0,0 +1,32 @@
+package com.fraud_detection.notification.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Entity
+@Table(name = "user_details")
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Builder
+@Getter
+
+
+public class UserDetailsEntity {
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+    @Column(name = "email_id")
+    private String emailId;
+    @Column(name = "account_number")
+    private double accountNumber;
+
+
+}
---
+++ src/main/java/com/fraud_detection/notification/service/NotificationService.java
@@ -0,0 +1,48 @@
+package com.fraud_detection.notification.service;
+
+import com.fraud_detection.notification.entity.UserDetailsEntity;
+import com.fraud_detection.notification.repository.NotificationRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public void sendNotification(String userId, String message, double accountNumber) {
+
+        // Logic to send notification
+//        "fetch email_id from db, using user_id and account_number";
+        log.info("Sending notification: {}", userId);
+        // Here you would implement the actual notification sending logic
+        // For example, sending an email or a push notification
+        UserDetailsEntity userDetails = notificationRepository.findByUserIdAndAccountNumber(userId, accountNumber);
+        String emailId = userDetails.getEmailId();
+        // Send email
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(emailId);
+        mailMessage.setSubject("Notification");
+        mailMessage.setText(message);
+
+        try {
+            mailSender.send(mailMessage);
+            log.info("Email sent successfully to {}", emailId);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}: {}", emailId, e.getMessage());
+        }
+    }
+}
+
+
